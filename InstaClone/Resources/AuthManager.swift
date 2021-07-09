13d5e6d72dac8d19f5b3ca9914e984caf43ff0fb
Radik Nuriev <shurale85@gmail.com>
2021-07-09 08:57:52 +0300

    signing in is implemented

--- InstaClone/Resources/AuthManager.swift
+++ InstaClone/Resources/AuthManager.swift
@@ -14,12 +14,50 @@ public class AuthManager {
     // MARK: - Public
     
     
-    public func registerNewUser(usernane: String, email: String, password: String){
+    public func registerNewUser(username: String, email: String, password: String, completion: @escaping (Bool) -> Void){
         
+        // Check if username is available
+        DatabaseManager.shared.canCreateNewUser(with: email, username: username) { canCreate in
+            if canCreate {
+                Auth.auth().createUser(withEmail: email, password: password) { (result, error) in
+                    guard error == nil, result != nil else {
+                        completion(false)
+                        return
+                    }
+                    
+                    //Inserting into DB
+                    DatabaseManager.shared.insertNewUser(with: email, username: username){ inserted in
+                        if inserted {
+                            completion(true)
+                            return
+                        }
+                        else {
+                            completion(false)
+                            return
+                        }
+                    }
+                }
+            } else {
+                completion(false)
+                return
+            }
+        }
     }
     
-    public func loginUser(username: String?, email: String?, password: String){
-        
+    public func loginUser(username: String?, email: String?, password: String, completion: @escaping (Bool) -> Void){
+        if let email = email {
+            Auth.auth().signIn(withEmail: email, password: password) { authResult, error in
+                guard authResult != nil, error == nil else {
+                    completion(false)
+                    return
+                }
+                
+                completion(true)
+            }
+        }
+        else if let username = username {
+            print(username)
+        }
     }
     
 }
--- InstaClone/Resources/DatabaseManager.swift
+++ InstaClone/Resources/DatabaseManager.swift
@@ -11,8 +11,41 @@ public class DatabaseManager {
     
     static let shared = DatabaseManager()
     
+    private let database =
+        Database.database(url: "https://instaclone-be8a7-default-rtdb.europe-west1.firebasedatabase.app").reference()
+    
     // MARK: - Public
 
+    /// Check if username and email is available
+    /// - Paramters
+    ///     - email: String representing email
+    ///     - username: String representing username
+    public func canCreateNewUser(with email: String, username: String, completion: (Bool) -> Void) {
+        
+        // Check if username is avaialble
+        completion(true)
+        
+    }
     
+    /// Insert new user into DB
+    /// - Paramters
+    ///     - email: String representing email
+    ///     - username: String representing username
+    ///     - completion: Async callback for result if database entry ssucceeded
+    public func insertNewUser(with email: String, username: String, completeion: @escaping (Bool) -> Void) {
+        database.child(email.safeDatabaseKey()).setValue(["username": username]) { error, _ in
+            if error == nil {
+                completeion(true)
+                return
+            }
+            else {
+                completeion(false)
+                return
+            }
+            
+        }
+        
+    }
     
+    // MARK: -Private
 }
